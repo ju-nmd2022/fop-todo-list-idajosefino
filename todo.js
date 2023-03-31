e6556f2bc1cceefa5536a7c00ef9aec28a7bba94
Ida <idajosefinolsson@gmail.com>
2023-03-31 18:54:35 +0200

    More styling + trying localStorage
    Struggling to get localStorage to work since I am not quite sure how to write it.

--- todo.js
+++ todo.js
@@ -1,10 +1,11 @@
+const InputText = document.getElementById('p'); 
+
 let addToDoButton = document.getElementById('addToDo');
 let toDoContainer = document.getElementById('toDoContainer');
 let inputField = document.getElementById('inputField');
 
 //event listener where you listen to the load event.
 function onLoadHandler() {
-    loadData();
     const headlineElement = document.querySelector("h1");
     const footerElement = document.getElementById("footer");
 }
@@ -15,11 +16,15 @@ addToDoButton.addEventListener('click', function(){
     paragraph.classList.add('paragraph-styling')
     paragraph.innerText = inputField.value;
     toDoContainer.appendChild(paragraph);
+//this is to make sure the inputfield is empty ones you have pressed +
     inputField.value = "";
+//when you click the to-do one time a line is drawn over it
     paragraph.addEventListener('click', function(){
         paragraph.style.textDecoration = "line-through";
     })
+//when you click twice on the to-do it disappears
     paragraph.addEventListener('dblclick', function(){
         toDoContainer.removeChild(paragraph);
+        localStorage.removeItem;
     })
 })
