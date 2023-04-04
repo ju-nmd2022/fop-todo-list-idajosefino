052b05fbaba27e2dd7302c9ebb5b9dc316430282
Ida <idajosefinolsson@gmail.com>
2023-04-04 19:12:44 +0200

    added this
    was taught about that this in a click-eventlistener. it is that element you click so i can change paragraph -> this in the click-eventlistener...

--- todo.js
+++ todo.js
@@ -1,3 +1,54 @@
+// const InputText = document.getElementById('p'); 
+
+// let addToDoButton = document.getElementById('addToDo');
+// let toDoContainer = document.getElementById('toDoContainer');
+// let inputField = document.getElementById('inputField');
+
+// let arrayOfTasks;
+
+// //how to read what is in localStorage
+// for (let i = 0; i < localStorage.length; i++) {
+//     var paragraph = document.createElement('p')
+//     paragraph.classList.add('paragraph-styling')
+//     paragraph.innerText = localStorage.key(i);
+//     toDoContainer.appendChild(paragraph);
+
+//     if (localStorage.getItem(localStorage.key(i)) === 'true') {
+//         paragraph.style.textDecoration = "line-through";
+//     }
+
+//     //when you click the to-do one time a line is drawn over it
+//     paragraph.addEventListener('click', function() {
+//         this.style.textDecoration = "line-through";
+//         localStorage.setItem(this.innerText, true);
+//     })
+//     //when you click twice on the to-do it disappears
+//     paragraph.addEventListener('dblclick', function() {
+//         localStorage.removeItem(this.innerHTML);
+//         toDoContainer.removeChild(this);
+//     })
+// }
+
+// addToDoButton.addEventListener('click', function(){
+//     var paragraph = document.createElement('p')
+//     paragraph.classList.add('paragraph-styling')
+//     paragraph.innerText = inputField.value;
+//     toDoContainer.appendChild(paragraph);
+// //this is to make sure the inputfield is empty ones you have pressed +
+//     inputField.value = "";
+//     localStorage.setItem(paragraph.innerText, false);
+// //when you click the to-do one time a line is drawn over it
+//     paragraph.addEventListener('click', function() {
+//         paragraph.style.textDecoration = "line-through";
+//         localStorage.setItem(paragraph.innerText, true);
+//     })
+// //when you click twice on the to-do it disappears
+//     paragraph.addEventListener('dblclick', function() {
+//         toDoContainer.removeChild(paragraph);
+//         localStorage.removeItem(paragraph.innerText, true);
+//     })
+// })
+
 const InputText = document.getElementById('p'); 
 
 let addToDoButton = document.getElementById('addToDo');
@@ -6,7 +57,6 @@ let inputField = document.getElementById('inputField');
 
 let arrayOfTasks;
 
-//how to read what is in localStorage
 for (let i = 0; i < localStorage.length; i++) {
     var paragraph = document.createElement('p')
     paragraph.classList.add('paragraph-styling')
@@ -18,36 +68,43 @@ for (let i = 0; i < localStorage.length; i++) {
     }
 
     //when you click the to-do one time a line is drawn over it
-    paragraph.addEventListener('click', function() {
+    paragraph.addEventListener('click', function(){
         this.style.textDecoration = "line-through";
         localStorage.setItem(this.innerText, true);
     })
     //when you click twice on the to-do it disappears
-    paragraph.addEventListener('dblclick', function() {
+    paragraph.addEventListener('dblclick', function(){
         localStorage.removeItem(this.innerHTML);
         toDoContainer.removeChild(this);
     })
 }
 
+//event listener where you listen to the load event.
+function onLoadHandler() {
+    const headlineElement = document.querySelector("h1");
+    const footerElement = document.getElementById("footer");
+}
+window.addEventListener("load", onLoadHandler);
+
 addToDoButton.addEventListener('click', function(){
     var paragraph = document.createElement('p')
     paragraph.classList.add('paragraph-styling')
     paragraph.innerText = inputField.value;
-    toDoContainer.appendChild(paragraph);
-//this is to make sure the inputfield is empty ones you have pressed +
+    toDoContainer.appendChild(paragraph); 
+    //this is to make sure the inputfield is empty ones you have pressed +
     inputField.value = "";
     localStorage.setItem(paragraph.innerText, false);
-//when you click the to-do one time a line is drawn over it
-    paragraph.addEventListener('click', function() {
-        paragraph.style.textDecoration = "line-through";
-        localStorage.setItem(paragraph.innerText, true);
+    //when you click the to-do one time a line is drawn over it
+    paragraph.addEventListener('click', function(){
+        this.style.textDecoration = "line-through";
+        localStorage.setItem(this.innerText, true);
     })
-//when you click twice on the to-do it disappears
-    paragraph.addEventListener('dblclick', function() {
-        toDoContainer.removeChild(paragraph);
-        localStorage.removeItem(paragraph.innerText, true);
+    //when you click twice on the to-do it disappears
+    paragraph.addEventListener('dblclick', function(){
+        localStorage.removeItem(this.innerHTML);
+        toDoContainer.removeChild(this);
     })
 })
 
+//need help with understanding how to add the paragraphs you write as to-dos is saved in localstorage and removed by double click...
 //need helo about onLoadHandler cause I am not sure it is working.
-
