1778c46b1e4e32abdf8af2ad7dc9bf7db9979487
Ida <idajosefinolsson@gmail.com>
2023-04-04 18:31:57 +0200

    localstorage works
    yes yes yes, it works.

--- todo.js
+++ todo.js
@@ -1,16 +1,33 @@
-localStorage.length;
 const InputText = document.getElementById('p'); 
 
 let addToDoButton = document.getElementById('addToDo');
 let toDoContainer = document.getElementById('toDoContainer');
 let inputField = document.getElementById('inputField');
 
-//event listener where you listen to the load event.
-function onLoadHandler() {
-    const headlineElement = document.querySelector("h1");
-    const footerElement = document.getElementById("footer");
+let arrayOfTasks;
+
+//how to read what is in localStorage
+for (let i = 0; i < localStorage.length; i++) {
+    var paragraph = document.createElement('p')
+    paragraph.classList.add('paragraph-styling')
+    paragraph.innerText = localStorage.key(i);
+    toDoContainer.appendChild(paragraph);
+
+    if (localStorage.getItem(localStorage.key(i)) === 'true') {
+        paragraph.style.textDecoration = "line-through";
+    }
+
+    //when you click the to-do one time a line is drawn over it
+    paragraph.addEventListener('click', function() {
+        this.style.textDecoration = "line-through";
+        localStorage.setItem(this.innerText, true);
+    })
+    //when you click twice on the to-do it disappears
+    paragraph.addEventListener('dblclick', function() {
+        localStorage.removeItem(this.innerHTML);
+        toDoContainer.removeChild(this);
+    })
 }
-window.addEventListener("load", onLoadHandler);
 
 addToDoButton.addEventListener('click', function(){
     var paragraph = document.createElement('p')
@@ -21,17 +38,16 @@ addToDoButton.addEventListener('click', function(){
     inputField.value = "";
     localStorage.setItem(paragraph.innerText, false);
 //when you click the to-do one time a line is drawn over it
-    paragraph.addEventListener('click', function(){
+    paragraph.addEventListener('click', function() {
         paragraph.style.textDecoration = "line-through";
         localStorage.setItem(paragraph.innerText, true);
     })
 //when you click twice on the to-do it disappears
-    paragraph.addEventListener('dblclick', function(){
+    paragraph.addEventListener('dblclick', function() {
         toDoContainer.removeChild(paragraph);
         localStorage.removeItem(paragraph.innerText, true);
     })
 })
 
-//need help with understanding how to add the paragraphs you write as to-dos is saved in localstorage and removed by double click...
 //need helo about onLoadHandler cause I am not sure it is working.
 
