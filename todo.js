b439ab153e9c409471ad850cd8d884a3130bfca2
Ida <idajosefinolsson@gmail.com>
2023-04-11 00:15:52 +0200

    code of conduct
    a better reference system set up cause i could never take the ultimate credit for something this pretty.

--- todo.js
+++ todo.js
@@ -1,3 +1,4 @@
+// The following 6 lines of code was adapted from https://www.youtube.com/watch?v=-pRg_daFjfk&list=WL&index=14 Accessed: 2023-03-31 and by courtesy of Garrit Schaap.
 //the getElementById() method of the document interface returns an element object representing the element whose id property matches the specified string. since element ids are required to be unique if specified, they're a useful way to get access to a specific element quickly.
 const InputText = document.getElementById('p'); 
 
@@ -5,7 +6,7 @@ let addToDoButton = document.getElementById('addToDo');
 let toDoContainer = document.getElementById('toDoContainer');
 let inputField = document.getElementById('inputField');
 
-//local storage
+// The following 25 lines of code was adapted from https://www.javatpoint.com/javascript-classlist and https://www.w3schools.com/html/html5_webstorage.asp Accessed: 2023-04-04 and by courtesy of Emma Åkerberg and Garrit Schaap.
 //length = the length read-only property of the Storage interface returns the number of data items stored in a given storage object.
 //classList and paragraph-styling = javaScript classList is a DOM property of javaScript that allows for styling the CSS classes of an element.
 for (let i = 0; i < localStorage.length; i++) {
@@ -17,7 +18,6 @@ for (let i = 0; i < localStorage.length; i++) {
     if (localStorage.getItem(localStorage.key(i)) === 'true') {
         paragraph.style.textDecoration = "line-through";
     }
-
     //when you click the to-do one time a line is drawn over it. "this" is used instead of "paragraph".
     // when you use this in a click eventListener it means that that is the element you click on.
     paragraph.addEventListener('click', function() {
@@ -32,6 +32,7 @@ for (let i = 0; i < localStorage.length; i++) {
 
 }
 
+// The following 19 lines of code was adapted from https://www.youtube.com/watch?v=-pRg_daFjfk&list=WL&index=14 Accessed: 2023-03-31 and by courtesy of Emma Åkerberg and Garrit Schaap.
 addToDoButton.addEventListener('click', function() {
     var paragraph = document.createElement('p')
     paragraph.classList.add('paragraph-styling')
